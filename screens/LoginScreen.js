684e9824da1886248e9fb59dad0c7e3c12913745
Nika Nozadze <nikanozadze1010@gmail.com>
2024-01-08 21:10:26 +0400

    feat: crate login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,28 @@
-import { Text, StyleSheet, View, Image, Button, FlatList } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 
 const LoginScreen = () => {
+  const { width: screenWidth } = Dimensions.get("window");
   return (
     <View style={styles.page}>
       <Text style={styles.title}>შესვლა</Text>
-      <Text></Text>
+      <Text style={styles.label}>ელ-ფოსტა</Text>
+      <TextInput
+        placeholder="Example@redberry.ge"
+        style={[styles.input, { width: screenWidth - 40 }]}
+      />
+      <TouchableOpacity
+        style={[styles.button, { width: screenWidth - 40 }]}
+        onPress={() => {}}
+      >
+        <Text style={styles.buttonText}>შესვლა</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,15 +34,37 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 28,
     color: "#1A1A1F",
-    marginTop: 10,
     fontWeight: "bold",
   },
   label: {
     fontSize: 20,
     color: "#1A1A1F",
+    marginRight: "auto",
+    marginTop: 30,
+  },
+  input: {
+    height: 44,
+    borderStyle: "solid",
+    borderWidth: 1.5,
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    borderRadius: 8,
+    backgroundColor: "#5D37F3",
+    marginTop: 30,
+    height: 40,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#ffffff",
   },
 });
