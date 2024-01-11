d770338a6792b90743c84e25ce1fae756be38852
Nika Nozadze <nikanozadze1010@gmail.com>
2024-01-12 00:37:00 +0400

    wip: work with image upload

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,3 @@
 export { default as Logo } from "./logo.png";
 export { default as Poster } from "./poster.png";
+export { default as Folder } from "./folder-add.png";
--- screens/NewBlogScreen.js
+++ screens/NewBlogScreen.js
@@ -1,24 +1,43 @@
-import { Text, StyleSheet, View, Image, Button, FlatList } from "react-native";
-import React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
+import { launchImageLibrary } from "react-native-image-picker";
 
 import { useCategory } from "../store";
-import { Logo } from "../assets";
+import { Folder, Logo } from "../assets";
 import newBlogSchema, { initialValues } from "../schemas/create-blog";
 
 const NewBlogScreen = () => {
   const categories = useCategory((state) => state.data);
   const navigation = useNavigation();
+  const [photo, setPhoto] = useState(null);
+
+  const handleChoosePhoto = () => {
+    launchImageLibrary({ noData: true }, (response) => {
+      if (response) {
+        setPhoto(response);
+      }
+    });
+  };
 
   return (
-    <View>
+    <View style={styles.page}>
       <View style={styles.header}>
         <Image source={Logo} />
       </View>
       <Text style={styles.title}>ბლოგის დამატება</Text>
       <Formik
         initialValues={initialValues}
-        onSubmit={submitHandler}
+        onSubmit={() => {}}
         validationSchema={newBlogSchema}
       >
         {({
@@ -28,7 +47,24 @@ const NewBlogScreen = () => {
           values,
           touched,
           errors,
-        }) => <View></View>}
+        }) => (
+          <View style={{ paddingHorizontal: 20 }}>
+            <Text style={styles.label}>ატვირთეთ ფოტო</Text>
+            {photo && (
+              <Image
+                source={{ uri: photo.uri }}
+                style={{ width: 300, height: 300 }}
+              />
+            )}
+            <TouchableOpacity
+              style={styles.imageBox}
+              onPress={handleChoosePhoto}
+            >
+              <Image source={Folder} />
+              <Text style={styles.label}>აირჩიეთ ფაილი</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </Formik>
     </View>
   );
@@ -37,6 +73,11 @@ const NewBlogScreen = () => {
 export default NewBlogScreen;
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    flexDirection: "column",
+    paddingVertical: 10,
+  },
   header: {
     width: "auto",
     backgroundColor: "#ffffff",
@@ -51,5 +92,22 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 20,
     textAlign: "center",
+  },
+  label: {
+    color: "#1A1A1F",
+    fontSize: 14,
+    fontWeight: "500",
+    marginTop: 40,
+  },
+  imageBox: {
+    borderStyle: "dashed",
+    borderWidth: 2,
+    paddingVertical: 24,
+    borderRadius: 30,
+    marginTop: 10,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
